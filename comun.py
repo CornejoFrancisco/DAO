92027422412a350d60387e4d7e4cea430cc403b7
julianbasilieri <julian_basilieri@hotmail.com>
2024-11-12 12:50:03 -0300

    reportes

--- comun.py
+++ comun.py
@@ -1,4 +1,6 @@
 from database import obtener_conexion
+from datetime import datetime, timedelta
+import locale
 
 def obtener_autores():
     """Función para obtener una lista de autores desde la base de datos."""
@@ -116,3 +118,106 @@ def actualizar_fecha_devolucion(prestamo_id, fecha_hoy):
 def cerrarVentana(ventana):
     """Función que cierra la ventana pasada como argumento."""
     ventana.destroy()
+    
+def prestamos_vencidos():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    fecha_actual = datetime.now().strftime('%Y-%m-%d')
+    print(fecha_actual)
+
+    # Obtener los préstamos cuya fecha de devolución sea menor a la fecha actual
+    cursor.execute("""
+        SELECT 
+            u.nombre || ' ' || u.apellido AS usuario, 
+            l.titulo AS libro, 
+            p.fecha_prestamo, 
+            julianday(?) - julianday(p.fecha_devolucion_estimada) AS dias_vencidos
+        FROM Prestamo p
+        JOIN Usuario u ON p.id = u.id
+        JOIN Libro l ON p.codigo_ISBN = l.codigo_ISBN
+        WHERE p.fecha_devolucion_estimada < ? 
+        AND p.fecha_devolucion_estimada IS NOT NULL 
+        AND p.fecha_devolucion_real IS NULL
+    """, (fecha_actual, fecha_actual))
+
+    prestamos_vencidos = cursor.fetchall()
+
+    # Cerrar la conexión
+    conexion.close()
+
+    # Si hay resultados, formatearlos como una tupla (encabezado, datos)
+    if prestamos_vencidos:
+        encabezado = ["Usuario", "Libro", "Fecha Préstamo", "Días Vencidos"]
+        datos = [(p[0], p[1], p[2], round(p[3])) for p in prestamos_vencidos]  # Calcular los días vencidos y redondear
+        return (encabezado, datos)
+    else:
+        # Si no hay resultados, devolver un encabezado vacío y una lista vacía
+        return (["Usuario", "Libro", "Fecha Préstamo", "Días Vencidos"], [])
+    
+def obtener_mes_anterior():
+    # Establecer la configuración regional a español
+    locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+
+    # Obtener la fecha actual
+    hoy = datetime.today()
+
+    # Restar un mes a la fecha actual
+    primer_dia_mes = hoy.replace(day=1)
+    ultimo_mes = primer_dia_mes - timedelta(days=1)
+
+    # Obtener el nombre del mes anterior en español
+    mes_anterior = ultimo_mes.strftime('%B')
+
+    return mes_anterior
+
+def libros_mas_prestados():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    # Obtener los libros más prestados en el último mes, agrupando por título y sumando la cantidad
+    cursor.execute("""
+        SELECT l.titulo, COUNT(p.codigo_ISBN) AS cantidad, l.genero
+        FROM Prestamo p
+        JOIN Libro l ON p.codigo_ISBN = l.codigo_ISBN
+        WHERE p.fecha_prestamo >= DATE('now', '-1 month')
+        GROUP BY l.titulo  -- Agrupar por título de libro
+        ORDER BY cantidad DESC
+    """)
+    libros_mas_prestados = cursor.fetchall()
+    conexion.close()
+
+    if libros_mas_prestados:
+        # Definir el encabezado
+        encabezado = ["Título del Libro", "Cantidad de Préstamos", "Genero"]
+        # El resultado contiene los títulos de los libros y la cantidad de préstamos
+        resultado = [(l[0], l[1], l[2]) for l in libros_mas_prestados]
+        return encabezado, resultado
+    else:
+        return ["Título del Libro", "Cantidad de Préstamos"], "No hay préstamos en el último mes."
+
+
+def usuarios_con_mas_prestamos():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    # Obtener los usuarios con más préstamos
+    cursor.execute("""
+        SELECT u.nombre, u.apellido, COUNT(p.usuario_id) AS cantidad
+        FROM Prestamo p
+        JOIN Usuario u ON p.usuario_id = u.id
+        GROUP BY p.usuario_id
+        ORDER BY cantidad DESC
+        LIMIT 5
+    """)
+    usuarios_con_mas_prestamos = cursor.fetchall()
+    conexion.close()
+
+    # Si hay resultados, devolverlos en el formato correcto
+    if usuarios_con_mas_prestamos:
+        encabezado = ["Usuario", "Cantidad de Préstamos"]
+        datos = [(f"{u[0]} {u[1]}", u[2]) for u in usuarios_con_mas_prestamos]
+        return (encabezado, datos)
+    else:
+        # Si no hay resultados, devolver un encabezado vacío y una lista vacía
+        return (["Usuario", "Cantidad de Préstamos"], [])
--- reportes.py
+++ reportes.py
@@ -1,6 +1,13 @@
 import tkinter as tk
-from clase import Prestamo
+from tkinter import messagebox
 from comun import *
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import  A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus.tables import Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
 
 def mostrar_opciones_reportes(self):
     # Crear una nueva ventana para los reportes
@@ -12,26 +19,64 @@ def mostrar_opciones_reportes(self):
     tk.Button(ventana_reportes, text="Usuarios con más préstamos", command=lambda: mostrar_usuarios_con_mas_prestamos(self)).pack(pady=10)
     tk.Button(ventana_reportes, text="Volver", command=lambda: cerrarVentana(ventana_reportes)).pack(pady=10)
 
-
 def mostrar_prestamos_vencidos(self):
-    report = Prestamo.prestamos_vencidos()
-    mostrar_resultado(self, report)
+    report = prestamos_vencidos()
+    mostrar_resultado(self, report, "prestamos vencidos", f"Prestamos vencidos")
 
 def mostrar_libros_mas_prestados(self):
-    report = Prestamo.libros_mas_prestados()
-    mostrar_resultado(self, report)
+    report = libros_mas_prestados()
+    mostrar_resultado(self, report, "libros mas prestados", f"Libros mas prestados de {obtener_mes_anterior()}")
 
 def mostrar_usuarios_con_mas_prestamos(self):
-    report = Prestamo.usuarios_con_mas_prestamos()
-    mostrar_resultado(self, report)
+    report = usuarios_con_mas_prestamos()
+    mostrar_resultado(self, report, "usuarios con mas prestamos", "Top 5 Usuarios con mas prestamos")
 
-def mostrar_resultado(self, resultado):
-    # Aquí se muestra la información en un widget de Tkinter
-    result_window = tk.Toplevel(self.root)  # Crea una nueva ventana para mostrar los resultados
-    result_window.title("Reporte")
+def mostrar_resultado(self, resultado, nombre, titulo):
+    # Crear un archivo PDF
+    pdf_file = nombre + ".pdf"
     
-    result_text = tk.Text(result_window, height=15, width=50)
-    result_text.pack(padx=10, pady=10)
+    # Crear un documento PDF
+    doc = SimpleDocTemplate(pdf_file, pagesize=A4)
     
-    result_text.insert(tk.END, resultado)
-    result_text.config(state=tk.DISABLED)  # Para que no se pueda editar el texto
+    # Crear una lista para almacenar los elementos del informe
+    elements = []
+    
+    # Crear estilos para el informe
+    styles = getSampleStyleSheet()
+    
+    elements.append(Paragraph(titulo, styles['Title']))
+    
+    # Crear la tabla con los datos
+    table_data = []
+    
+    # Encabezado de la tabla
+    encabezado, datos = resultado  # resultado es una tupla (encabezado, datos)
+    table_data.append(encabezado)  # Añadir encabezado
+    
+    # Añadir las filas de datos
+    for item in datos:
+        table_data.append(item)  # Cada fila es una tupla (título del libro, cantidad)
+    
+    # Crear la tabla con los datos
+    table = Table(table_data)
+
+    # Estilo de la tabla (puedes personalizarlo según tus necesidades)
+    style = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.lightslategray),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ])
+    
+    table.setStyle(style)
+    elements.append(Spacer(10, 10))
+    elements.append(table)
+
+    # Construir el documento con el título y la tabla
+    doc.build(elements)
+
+    # Notificar al usuario que el PDF ha sido generado
+    messagebox.showinfo("Generar PDF", f"El reporte ha sido generado y guardado como: \"{pdf_file}\"")
